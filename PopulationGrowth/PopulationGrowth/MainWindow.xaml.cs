196220c5fe1d893ce023ff5a4d32ee2c4f022a1a
Gurleen Singh <42860318+GurleenSinghIITR@users.noreply.github.com>
2018-10-05 16:43:54 -0400

    Population Growth Application with initial number of organisms, birthrate, deathrate and randomized catastrophies
    Application that predicts the approximate size of a population of organisms allowing the user to enter the starting number of organisms, the average daily population increase (as a percentage), and the number of days the organisms will be left to multiply.
    
    Used a ListBox to display the population size for each day of the simulation.
    
    If the user clicks the Save button, save the current prediction to a file.

---
+++ PopulationGrowth/PopulationGrowth/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PopulationGrowth
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Vm vm = new Vm();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+
+        private void Calc_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Shew();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Kill_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Shoot();
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Resets();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Saves();
+        }
+
+        private void InPopulation_PreviewTextInput(object sender, TextCompositionEventArgs e)   /// Validations for the input to be a number
+        {
+            bool isInputTextNumber = new Regex("[0-9]").IsMatch(e.Text);
+            e.Handled = !isInputTextNumber;
+        }
+
+        private void PopulationIncreaseRate_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            bool isInputTextNumber = new Regex("[0-9]").IsMatch(e.Text);
+            e.Handled = !isInputTextNumber;
+        }
+
+        private void DaysToCalculate_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            bool isInputTextNumber = new Regex("[0-9]").IsMatch(e.Text);
+            e.Handled = !isInputTextNumber;
+        }
+
+        private void MortalityRatePerDay_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            bool isInputTextNumber = new Regex("[0-9]").IsMatch(e.Text);
+            e.Handled = !isInputTextNumber;
+        }
+    }
+}
---
+++ PopulationGrowth/PopulationGrowth/Vm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopulationGrowth
+{
+    class Vm : INotifyPropertyChanged
+    {
+        public const decimal CENT = 100;   
+        decimal count;
+        public decimal Count { get { return count; } set { count = value; NotifyChange(); } }
+
+        decimal rate;
+        public decimal Rate { get { return rate; } set { rate = value; NotifyChange(); } }
+
+        decimal days;
+        public decimal Days { get { return days; } set { days = value; NotifyChange(); } }
+
+        decimal death;
+        public decimal Death { get { return death; } set { death = value; NotifyChange(); } }
+
+        string disaster;
+        public string Disaster { get { return disaster; } set { disaster = value; NotifyChange(); } }
+
+        object destruct;
+        public Object Destruct { get { return destruct; } set { destruct = value; NotifyChange(); } }
+
+        List<People> peeps = new List<People>();    /// Binding of the Listbox Item Source
+        public List<People> Peeps
+        { get { return peeps; }  set { peeps = value; NotifyChange(); } }
+
+        
+        public void Shew()
+        {
+            List<People> _peeps = new List<People>();
+            decimal Counts = Count;
+            for (int i = 0; i < Days; i++)  ///Caculate with birth and death rate
+            {
+                Counts += Counts * (Rate - Death) / CENT;
+
+                People thing = new People
+                {
+                    Message = $"Population on day {i+1} is",
+                    Value = Math.Round(Counts)
+                };
+                _peeps.Add(thing);
+            }
+            Peeps = _peeps;
+        }
+
+        public void Shoot()  /// Method for the Catastrophe Click Button
+        {
+            decimal dToll=0;
+            Random r = new Random();    /// Random Integer to introduce Disasters
+            int deads = r.Next(1, 4);
+            switch(deads)
+            {
+                case 1:
+                    dToll = 5;
+                    Disaster = "Earthquake will kill at 5% Rate";
+                    Destruct = "Disaster1.jpg";
+                    break;
+                case 2:
+                    dToll = 10;
+                    Disaster = "Hurricane will kill at 10% rate";
+                    Destruct = "Disaster2.jpg";
+                    break;
+                case 3:
+                    dToll = 15;
+                    Disaster = "Plague with kill at 15% rate";
+                    Destruct = "Disaster3.jpg";
+                    break;
+            }
+            List<People> _peeps = new List<People>();
+            decimal Counts = Count;
+            for (int i = 0; i < Days; i++)  ///Calculate with the birth rate ,death rate and death toll
+            {
+                Counts += Counts * (Rate - Death - dToll) / CENT;
+
+                People thing = new People
+                {
+                    Message = $"Population on day {i + 1}",
+                    Value = Math.Round(Counts)
+                };
+                _peeps.Add(thing);
+            }
+            Peeps = _peeps;
+        }
+
+        public void Resets()    ///resets everything
+        {
+            Count = 0;
+            Days = 0;
+            Death = 0;
+            Rate = 0;
+            Disaster = " ";
+            Destruct = " ";
+            Peeps = null;
+        }
+
+        public void Saves() /// saves it to the file output.txt
+        {
+            string saveString = "";
+            foreach (People  thing in Peeps)
+                saveString += thing.ToString();
+            File.AppendAllText("output.txt", saveString);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyChange([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
